5ed2b526f49d4911cb6bfb7c95efde436e331ff7
karl-otto <karl-otto@towebo.se>
2017-02-05 23:20:49 +0100

    Written an own deselializer for GraphML because the one that comes along is dependant on System.Reflection.Emit which is banned from iOS.

--- GraphML-Test/Models/WFGraph.cs
+++ GraphML-Test/Models/WFGraph.cs
@@ -1,5 +1,4 @@
 ﻿using QuickGraph;
-using QuickGraph.Serialization.Mawingu;
 using System;
 using System.IO;
 using System.Xml;
@@ -8,12 +7,16 @@ using System.Linq;
 using System.Collections.Generic;
 using QuickGraph.Algorithms.ShortestPath;
 using QuickGraph.Algorithms.Observers;
+using System.Xml.Linq;
+using System.Reflection;
+using System.Globalization;
 
 namespace WayfindR.Models
 {
     [Serializable]
     public class WFGraph : AdjacencyGraph<WFNode, WFEdge<WFNode>>
-    {
+    {        
+
         public string Id { get; set; }
 
         [XmlAttribute("venue_id")]
@@ -24,37 +27,303 @@ namespace WayfindR.Models
 
         [XmlAttribute("graph_level")]
         public int Level { get; set; }
-        
+
+
+
+
+        public WFGraph()
+        {
+        }
+
+
+        private static Dictionary<string, string> MakeDictionaryOfTheseElements(IEnumerable<XElement> dataElements, List<GraphMLKey> gKeys)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            foreach (XElement data in dataElements)
+            {
+                string name = NodeAttribute(data, "key");
+                string val = data.Value;
+
+                GraphMLKey gk = gKeys.Where(w => w.Id == name).FirstOrDefault();
+                if (gk != null)
+                {
+                    name = gk.Name;
+                    if (string.IsNullOrEmpty(val))
+                    {
+                        val = gk.DefaultValue;
+
+                    } // No value
+
+                } // gk not null
+
+                if (!string.IsNullOrEmpty(name))
+                {
+                    result[name] = val;
+
+                }
+                
+
+            } // foreach
+
+            return result;
+
+        }
+
+        public static bool TryGetAttributeName(PropertyInfo property, out string name)
+        {
+            var attribute = Attribute.GetCustomAttribute(property, typeof(XmlAttributeAttribute))
+                as XmlAttributeAttribute;
+            if (attribute == null)
+            {
+                name = null;
+                return false;
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(attribute.AttributeName))
+                {
+                    name = property.Name;
+                }
+                else
+                {
+                    name = attribute.AttributeName;
+
+                }
+
+                return true;
+            }
+        }
+
+
+        private static void SetPropertyValues(object obj, Dictionary<string, string> dataDict)
+        {
+            try
+            {
+                Type currentType = obj.GetType();
+
+                foreach (PropertyInfo prop in currentType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {                    
+                    if (!prop.CanWrite || prop.GetIndexParameters().Length > 0)
+                    {
+                        continue;
+
+                    }
+
+                    string propname = prop.Name;
+                    
+                    // Is it tagged with XmlAttributeAttribute?
+                    string xmlname;
+                    if (TryGetAttributeName(prop, out xmlname))
+                    {
+                        propname = xmlname;
+                                                
+                    } // XmlAttr
+
+                    if (dataDict.ContainsKey(propname))
+                    {
+                        try
+                        {
+                            string sval = dataDict[propname];
+
+                            if (prop.PropertyType == typeof(int))
+                            {
+                                int ival;
+                                if (int.TryParse(sval, out ival))
+                                {
+                                    prop.SetValue(obj, ival);
+
+                                }                                
+
+                            }
+                            else if (prop.PropertyType == typeof(double))
+                            {
+                                double dval;
+                                if (double.TryParse(
+                                    sval, 
+                                    NumberStyles.Number | NumberStyles.AllowDecimalPoint,
+                                    CultureInfo.CreateSpecificCulture("en-US"),
+                                    out dval))
+                                {
+                                    prop.SetValue(obj, dval);
+
+                                }
+                            }
+                            else
+                            {
+                                prop.SetValue(obj, dataDict[propname]);
+
+                            }
+                            
+
+                        }
+                        catch
+                        {
+                            string nils = "";
+                        }
+                        
+
+                    } // Found in dictionary
+
+
+                } // foreach
+
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+
+            }
+
+        }
+
+
+
+
+        private static string NodeValue(XElement node, string nodeName, XNamespace ns)
+        {
+            if (node == null)
+            {
+                return "";
+
+            }
+
+            XElement valnode = node.Element(ns + nodeName);
+            if (valnode == null)
+            {
+                return "";
+
+            }
+
+            return valnode.Value;
+
+        }
+
+        private static string SubNodeValue(XElement node, string nodeName, string subNodeName, XNamespace ns)
+        {
+            if (node == null)
+            {
+                return "";
+
+            }
+
+            XElement childnode = node.Element(ns + nodeName);
+            if (childnode == null)
+            {
+                return "";
+
+            }
+
+            XElement subnode = childnode.Element(ns + subNodeName);
+            if (subnode == null)
+            {
+                return "";
+            }
+
+            return subnode.Value;
+
+        }
+
+
+        private static string NodeAttribute(XElement node, string attribName)
+        {
+            if (node == null)
+            {
+                return "";
+
+            }
+
+            XAttribute attrib = node.Attribute(attribName);
+            if (attrib == null)
+            {
+                return "";
+
+            }
+
+            return attrib.Value;
+
+        }
+
 
         public static WFGraph LoadFromGraphML(Stream stream)
         {
             try
             {
-                WFGraph result = new WFGraph();
+                WFGraph result = null;
+                List<GraphMLKey> gklist = new List<GraphMLKey>();
 
+                XDocument xdoc = XDocument.Load(stream);
+                XElement xroot = xdoc.Root;
+                XNamespace ns = xroot.GetDefaultNamespace();
                 
+                // Attributes
+                var attrkeys = xroot.Elements(ns + "key");
 
-                using (var xreader = XmlReader.Create(stream))
+                foreach (XElement attrkey in attrkeys)
                 {
-                    try
+                    GraphMLKey gk = new GraphMLKey()
                     {
-                            result.DeserializeFromGraphML(xreader,
-                            (id) => new WFNode(id),
-                            (source, target, id) => new WFEdge<WFNode>(source, target, id)
-                            );
-                        
+                        Name = NodeAttribute(attrkey, "attr.name"),
+                        DataType = NodeAttribute(attrkey, "attr.type"),
+                        Id = NodeAttribute(attrkey, "id"),
+                        ForType = NodeAttribute(attrkey, "for"),
+                        DefaultValue = NodeValue(attrkey, "default", ns)
+                    }; // new
 
-                        return result;
+                    gklist.Add(gk);
 
-                    }
-                    finally
+                } // foreach attribute key
+
+                XElement xgraph = xroot.Element(ns + "graph");
+                if (xgraph != null)
+                {
+                    result = new WFGraph();
+
+                    // Graph properties
+                    var xgdata = xgraph.Elements(ns + "data");
+                    SetPropertyValues(result, MakeDictionaryOfTheseElements(xgdata, gklist));
+
+                    // Nodes
+                    Dictionary<string, WFNode> gnodes = new Dictionary<string, WFNode>();
+                    var xnodes = xgraph.Elements(ns + "node");
+                    foreach (XElement xnode in xnodes)
                     {
-                        xreader.Close();
-                    } // try
+                        string nid = NodeAttribute(xnode, "id");
+                        WFNode wfn = new WFNode(nid);
 
-                } // using
+                        var xndata = xnode.Elements(ns + "data");
+                        SetPropertyValues(wfn, MakeDictionaryOfTheseElements(xndata, gklist));
+
+                        result.AddVertex(wfn);
+                        gnodes[nid] = wfn;
+
+                    } // foreach xnode
 
 
+                    // Edges
+                    var xedges = xgraph.Elements(ns + "edge");
+                    foreach (XElement xedge in xedges)
+                    {
+                        string eid = NodeAttribute(xedge, "id");
+                        string src = NodeAttribute(xedge, "source");
+                        string tgt = NodeAttribute(xedge, "target");
+                        WFNode srcnode = gnodes[src];
+                        WFNode tgtnode = gnodes[tgt];
+
+                        WFEdge<WFNode> wfe = new WFEdge<WFNode>(srcnode, tgtnode, eid);
+
+                        var xedata = xedge.Elements(ns + "data");
+                        SetPropertyValues(wfe, MakeDictionaryOfTheseElements(xedata, gklist));
+
+                        result.AddEdge(wfe);
+
+                    } // foreach xedge
+                    
+                } // if xgraph not null
+                
+                    return result;
+                
             }
             catch (Exception ex)
             {
@@ -117,6 +386,18 @@ namespace WayfindR.Models
 
         }
 
-    }
+    } // class WFGraph
+
+
+    public class GraphMLKey
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public string ForType { get; set; }
+        public string DefaultValue { get; set; }
+
+
+    } // GraphMLKey
 
 }
