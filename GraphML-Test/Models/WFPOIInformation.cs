9929a37fe690be5c7b6bfd764d58d1883cb99dfa
karl-otto <karl-otto@towebo.se>
2017-03-10 14:19:27 +0100

    Visual Studio didn't automatically add these files to the repo.

---
+++ GraphML-Test/Models/WFPOIInformation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WayfindR.Models
+{
+    public class WFPOIInformation
+    {
+        public enum InfoCategory
+        {
+            Description,
+            Interior,
+            OpeningHours,
+            Offer
+        }
+
+
+        public string Information { get; set; }
+        public InfoCategory Category { get; set; }
+
+
+        public InfoCategory CategoryFromString(string catName)
+        {
+            try
+            {
+                if ("sescription" == catName.ToLower())
+                {
+                    return InfoCategory.Description;
+                }
+                else if ("interior" == catName.ToLower())
+                {
+                    return InfoCategory.Interior;
+                }
+                else if ("openinghours" == catName.ToLower())
+                {
+                    return InfoCategory.OpeningHours;
+                }
+                else if ("offer" == catName.ToLower())
+                {
+                    return InfoCategory.Offer;
+                }
+
+                return InfoCategory.Description;
+
+            }
+            catch
+            {
+                return InfoCategory.Description;
+
+            }
+
+        }
+
+    }
+}
---
+++ GraphML-Test/Models/WFPointOfInterest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WayfindR.Models
+{
+    public class WFPointOfInterest
+    {
+        public enum Category
+        {
+            General,
+            WC,
+            HWC
+
+        } // enum
+
+        public string Name { get; set; }
+        public string DescriptiveName { get; set; }
+
+        public string BeaconUuid { get; set; }
+        public int BeaconMajor { get; set; }
+        public int BeaconMinor { get; set; }
+
+
+        public WFPOIInformation[] Information { get; set; }
+
+
+        public Category CategoryFromString(string catName)
+        {
+            if ("general" == catName.ToLower())
+            {
+                return Category.General;
+            }
+            else if ("wc" == catName.ToLower())
+            {
+                return Category.WC;
+            }
+            else if ("hwc" == catName.ToLower())
+            {
+                return Category.HWC;
+            }
+
+            return Category.General;
+
+        }
+
+
+    }
+}
