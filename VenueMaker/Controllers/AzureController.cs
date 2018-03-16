2a3daa6e92e55dbff1c4291a61963f6a6c65461f
Karl-Otto Rosenqvist <karl-otto@towebo.se>
2018-03-16 23:41:06 +0100

    Added the Kwenda app icon and bumped the revision.

---
+++ VenueMaker/Controllers/AzureController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using VenueMaker.Models;
+
+namespace VenueMaker.Controllers
+{
+    public class AzureController
+    {
+
+        public static MobileServiceClient MobileService = new MobileServiceClient(
+            "https://kwenda.azurewebsites.net"
+        );
+
+
+        public async static void Populate()
+        {
+            DataFile df = new DataFile();
+            df.FileName = "Towebo.graphml";
+            df.VenueId = "0431453084";
+            df.FileExt = "graphml";
+
+            df.LastModified = DateTime.Now;
+            df.FileSize = 1024;
+
+            await MobileService.GetTable<DataFile>().InsertAsync(df);
+
+        }
+
+        public async static void haveALook()
+        {
+            IMobileServiceTable<DataFile> filesTable = MobileService.GetTable<DataFile>();
+            var items = await filesTable.ToListAsync();
+            foreach (DataFile df in items)
+            {
+                string fn = df.FileName;
+                if (string.IsNullOrWhiteSpace(fn))
+                {
+                    fn = "Harry";
+                }
+            }
+
+
+        }
+
+
+
+
+
+    }
+
+}
---
+++ VenueMaker/Models/DataFile.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VenueMaker.Models
+{
+
+    public class DataFile
+    {
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "venueid")]
+        public string VenueId { get; set; }
+
+        [JsonProperty(PropertyName = "filename")]
+        public string FileName { get; set; }
+
+        [JsonProperty(PropertyName = "fileext")]
+        public string FileExt { get; set; }
+
+        [JsonProperty(PropertyName = "lastmodified")]
+        public DateTime LastModified { get; set; }
+
+        [JsonProperty(PropertyName = "filesize")]
+        public long FileSize { get; set; }
+
+
+
+    }
+}
